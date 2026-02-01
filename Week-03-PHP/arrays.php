7a473da5ce46e7d8d7955bb3aceee89f673ac1ef
Kiera Gross <Kiera.Gross@Gmail.com>
2026-01-31 23:30:07 -0500

    feat: added stuff from "nested arrays" section of lecture

--- Week-03-PHP/arrays.php
+++ Week-03-PHP/arrays.php
@@ -4,6 +4,14 @@ $myarray = array(1,2,3);
 array_push($myarray, 4);
 $myarray[5] = 6;
 
-var_dump($myarray[1000]);
+# var_dump($myarray[1000]);
+
+$mypets = array("cats" => array("Zelda"), "dragons" => array("Puff", "Smaug", "Tiamat"));
+
+$mypetstable = array(array("name" => "Zelda", "type" => "cat", "age" => 9, "color" => "buff"), array("name" => "Puff", "type" => "dragon", "age" => 65, "color" => "green"));
+
+# var_dump($mypets["cats"][0]);
+
+# var_dump($mypetstable[0]["name"]);
 
 ?>
