74ea2b9d006fbbb787696d3dafd6acc7f503ef37
Kiera Gross <Kiera.Gross@Gmail.com>
2026-02-01 17:36:56 -0500

    feat: added errors file, added for loops from lecture

--- Week-03-PHP/loops.php
+++ Week-03-PHP/loops.php
@@ -9,4 +9,30 @@ while ($mycount > 0){
 
 echo "Happy New Year!";
 
+echo " for loop 1: ";
+
+$myarray = array("a","b","c");
+for ($myindex = 0; $myindex < count($myarray); $myindex++){
+    echo $myarray[$myindex];
+}
+
+echo " foreach loop 1: ";
+
+foreach ($myarray as $value){
+    echo $value;
+}
+
+echo " for loop 2: ";
+
+for ($mycount = 10; $mycount; $mycount--){
+    echo $mycount;
+}
+
+echo " foreach loop 2: ";
+
+$mycount = array(1,2,3,4,5,6,7,8,9,10);
+foreach ($mycount as $number){
+    echo $number;
+}
+
 ?>
