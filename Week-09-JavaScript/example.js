f372bc569f213f003bdb5c55dfc647228c2c7bf2
Kiera Gross <Kiera.Gross@Gmail.com>
2026-03-24 22:28:38 -0400

    feat: week 9 material

---
+++ Week-09-JavaScript/example.js
@@ -0,0 +1,5 @@
+console.log("Hello World");
+
+const myarray = [1,2,3,4,5];
+
+// output: [1,2,4,5]
---
+++ Week-09-JavaScript/examplewebpage.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<meta charset="utf-8" />
+<title>My Testing JS Page</title>
+<script src="example.js"></script>
+</head>
+<body>
+    <main>
+        <p>Testing JS Stuff</p>
+    </main>
+</body>
+</html>
