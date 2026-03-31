d9247c751164a632e3a44e1f1f5dc6a09b2cc5d5
Kiera Gross <Kiera.Gross@Gmail.com>
2026-03-31 17:10:55 -0400

    feat: added event listener examples

---
+++ Week-10-JavaScript/eventlisteners.js
@@ -0,0 +1,29 @@
+const firstButton = document.querySelector("#button-id");
+
+firstButton.addEventListener("click", () => {
+    console.log("Hello!");
+}
+    );
+
+const radioList = document.querySelectorAll("[type=radio]");
+
+console.log(radioList);
+
+const body = document.querySelector("body");
+
+for(const radioButton of radioList){
+    radioButton.addEventListener("focus", () => {
+        const color = radioButton.value;
+        body.style.setProperty("background-color", color);
+    });
+    radioButton.addEventListener("blur", () => {
+        body.style.setProperty("background-color", "white");
+    });
+}
+
+// const checkbox = document.querySelector("[type=checkbox]");
+
+// checkbox.addEventListener("click", (event) => {
+//     event.preventDefault();
+//     console.log("yes");
+// });
